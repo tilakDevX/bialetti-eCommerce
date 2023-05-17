236b0d88eda4f410e577ab5c29986aa7f80025db
RapidKS <sharma.kaush120375@gmail.com>
2023-05-17 23:33:49 +0530

    navbar sign in add

---
+++ navbar/navbar.js
@@ -0,0 +1,39 @@
+var x=document.getElementById('login');
+var y=document.getElementById('register');
+var z=document.getElementById('btn');
+function register()
+{
+    x.style.left='-400px';
+    y.style.left='50px';
+    z.style.left='110px';
+}
+function login()
+{
+    x.style.left='50px';
+    y.style.left='450px';
+    z.style.left='0px';
+}
+var modal = document.getElementById('login-form');
+window.onclick = function(event) 
+{
+    if (event.target == modal) 
+    {
+        document.getElementById("login-form").style.display = "none";
+    }
+}  
+
+document.querySelector(".submit-btn").addEventListener("click", display);
+
+
+
+function display(){
+   const close = document.getElementById("login-form");
+
+   close.style.display = "none";
+}
+
+
+
+
+
+
