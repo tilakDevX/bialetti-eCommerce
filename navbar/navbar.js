1cb12b86868e1c2086542daff579623e789c17ba
RapidKS <sharma.kaush120375@gmail.com>
2023-05-19 13:03:44 +0530

    navbar-sidebar

--- navbar/navbar.js
+++ navbar/navbar.js
@@ -124,5 +124,11 @@ function displaysearch(){
 }
 
 
-
+function w3_open() {
+    document.getElementById("mySidebar").style.display = "flex";
+  }
+  
+  function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+  }
 
